436be45c13c4dff62fc8b490ac5abd5c00728cdf
Khaled-Walid <justkhwf@outlook.com>
2021-10-03 17:23:32 +0200

    refactor conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,24 +15,25 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === selectedYear;
   });
 
+  let expensesContent = <p>No Expenses to show.</p>;
+
+  if (filteredList.length > 0) {
+    expensesContent = filteredList.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={selectedYear}
         onDropdownChange={dropdownYearHandler}
       ></ExpensesFilter>
-      {filteredList.length === 0 ? (
-        <p>No Expenses to show.</p>
-      ) : (
-        filteredList.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )}
+      {expensesContent}
     </Card>
   );
 }
