fb22ffbf3911be221c968f0f9a3198b4ece9cf45
Khaled-Walid <justkhwf@outlook.com>
2021-10-03 19:14:15 +0200

    make NewExpense collapsible

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 function NewExpense(props) {
+  const [collapsed, setCollapsed] = useState(true);
+
   function saveExpenseDataHandler(enteredExpenseData) {
     const expenseData = { ...enteredExpenseData, id: Math.random() };
     props.onAddExpense(expenseData);
+    setCollapsed(true);
   }
+
+  function collapseHandler() {
+    setCollapsed(true);
+  }
+  function uncollapseHandler() {
+    setCollapsed(false);
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {collapsed && (
+        <button onClick={uncollapseHandler}>Add New Expense</button>
+      )}
+      {!collapsed && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={collapseHandler}
+        />
+      )}
     </div>
   );
 }
